3117ac7ccc2fe2f5d3868030a0df4b13a5b4d420
TrueJustNeon <NeonWasHere25@gmail.com>
2026-02-26 15:06:18 -0300

    added more items!

--- kubejs/startup_scripts/registry/tools.js
+++ kubejs/startup_scripts/registry/tools.js
@@ -1,1 +1,18 @@
 // Items like, screwdrivers, with durability
+
+onEvent('item.registry.tool_tiers', event => {
+    event.add('tool', tier => {
+        tier.uses = 250
+        tier.repairIngredient = '#forge:ingots/iron'
+    })
+});
+
+onEvent('item.registry', (event) => {
+    let tool = (name,rarity) => {
+        let id = name.toLowerCase()
+        event.create(id, 'sword').tier('tool').texture("kubejs:item/" + id).displayName(name).rarity(RARITY_COMMON)
+    }
+tool('Screwdriver')
+tool('Hammer'
+
+}
