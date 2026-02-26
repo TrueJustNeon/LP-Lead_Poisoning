43727b0e637337d171a550f36980d776645d998d
TrueJustNeon <NeonWasHere25@gmail.com>
2026-02-26 12:37:30 -0300

    added mechanism recipes and images + mod folder.

--- kubejs/startup_scripts/registry/mechanisms.js
+++ kubejs/startup_scripts/registry/mechanisms.js
@@ -1,1 +1,17 @@
+onEvent('item.registry', (event) => {
 
+    let mechanism = (name, rarity) => {
+        let id = name.toLowerCase()
+        event.create(id + '_mechanism').texture("kubejs:item/" + id + "_mechanism").displayName(name + ' Mechanism').rarity(RARITY_COMMON)
+        event.create('incomplete_' + id + '_mechanism', 'create:sequenced_assembly').texture("kubejs:item/incomplete_" + id + "_mechanism").displayName('Incomplete ' + name + ' Mechanism')
+
+    }
+
+mechanism('Wooden')
+mechanism('Flint')
+mechanism('Andesite')
+mechanism('Brass')
+mechanism('Steel')
+mechanism('HDPE')
+mechanism('Tungsten')
+}
