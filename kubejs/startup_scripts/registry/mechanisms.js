081b7a756454405f1b5d81ee0bd09ab08a3c4fe8
TrueJustNeon <NeonWasHere25@gmail.com>
2026-02-27 16:37:59 -0300

    removed .rarity(COMMON)

--- kubejs/startup_scripts/registry/mechanisms.js
+++ kubejs/startup_scripts/registry/mechanisms.js
@@ -1,8 +1,8 @@
 StartupEvents.registry('item', event => {
 
-    let mechanism = (name, rarity) => {
+    let mechanism = (name) => {
         let id = name.toLowerCase()
-        event.create(id + '_mechanism').texture("kubejs:item/" + id + "_mechanism").displayName(name + ' Mechanism').rarity(COMMON)
+        event.create(id + '_mechanism').texture("kubejs:item/" + id + "_mechanism").displayName(name + ' Mechanism')
         event.create('incomplete_' + id + '_mechanism', 'create:sequenced_assembly').texture("kubejs:item/incomplete_" + id + "_mechanism").displayName('Incomplete ' + name + ' Mechanism')
 
     }
