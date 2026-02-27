aa0a059a669d0586e863375bb0fd5b12f7eb70fc
TrueJustNeon <NeonWasHere25@gmail.com>
2026-02-27 16:33:39 -0300

    startup mechanism fix

--- kubejs/startup_scripts/registry/mechanisms.js
+++ kubejs/startup_scripts/registry/mechanisms.js
@@ -2,7 +2,7 @@ StartupEvents.registry('item', event => {
 
     let mechanism = (name, rarity) => {
         let id = name.toLowerCase()
-        event.create(id + '_mechanism').texture("kubejs:item/" + id + "_mechanism").displayName(name + ' Mechanism').rarity(RARITY_COMMON)
+        event.create(id + '_mechanism').texture("kubejs:item/" + id + "_mechanism").displayName(name + ' Mechanism').rarity(COMMON)
         event.create('incomplete_' + id + '_mechanism', 'create:sequenced_assembly').texture("kubejs:item/incomplete_" + id + "_mechanism").displayName('Incomplete ' + name + ' Mechanism')
 
     }
