2aef2fdcebf88990db93f5cd34acd06e8a6a7421
TrueJustNeon <160098253+TrueJustNeon@users.noreply.github.com>
2026-03-02 23:58:13 -0300

    Actualizar chainmail_leggings.js

--- kubejs/server_scripts/recipes/sequenced_assemblies/minecraft/chainmail_leggings.js
+++ kubejs/server_scripts/recipes/sequenced_assemblies/minecraft/chainmail_leggings.js
@@ -4,9 +4,9 @@ let CR = (id, x) => MOD("create", id, x)
 let MC = (id, x) => MOD("minecraft", id, x)
 
 ServerEvents.recipes(event => {
-  t = KJ('incomplete_chainmail_leggings')
+  const t = KJ('incomplete_chainmail_leggings')
  event.recipes.createSequencedAssembly([
-    'minecraft:chainmail_leggings', // Output
+    Item.of('minecraft:chainmail_leggings'), // Output
   ], MC('leather_leggings'), [
     event.recipes.createDeploying(t, [t, 'create_tank_defenses:steel_sheet']), // Steel sheet
     event.recipes.create.filling(t, [t, Fluid.of('minecraft:lava', 50)]), // Lava
